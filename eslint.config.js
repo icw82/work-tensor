105fb268008320e7b523b4c0d615449437f02af0
icw82 <icw82@yandex.ru>
2023-12-12 12:03:25 +0500

    init

---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import { all } from './lint-configs/eslint/eslint.config.mjs';
+import { es5 } from './lint-configs/eslint/es5.config.mjs';
+
+
+export default [
+    ...all,
+    {
+        files: [
+            '*.config.js',
+            'client/**/*.{js,ts,jsx,tsx}',
+        ],
+    }, {
+        files: [
+            'client/**/*.ts',
+        ],
+        languageOptions: {
+            parserOptions: {
+                project: './client/tsconfig.eslint.json',
+            },
+        },
+        rules: {
+            '@typescript-eslint/comma-dangle': [
+                'error',
+                'never',
+            ],
+        },
+    }, {
+        files: [
+            'client/**/*.tsx',
+        ],
+        languageOptions: {
+            parserOptions: {
+                project: './client/tsconfig.eslint.json',
+            },
+        },
+        // rules: {
+        //     '@typescript-eslint/comma-dangle': [
+        //         'error',
+        //         'never',
+        //     ],
+        // },
+    }, {
+        files: ['client/**/*.js'],
+        ignores: ['client/**/*.router.js'],
+        ...es5,
+    }, {
+        ignores: [ '*.config.js' ],
+        // rules: {
+        //     'no-console': 2,
+        //     'comma-dangle': [
+        //         'error',
+        //         'never',
+        //     ],
+        // },
+    },
+];
