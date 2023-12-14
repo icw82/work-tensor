80db2c6fad4bd09d13077670c30913c1a286a241
icw82 <icw82@yandex.ru>
2023-12-14 21:45:25 +0500

    update

---
+++ app/locals/jest.ts
@@ -0,0 +1,77 @@
+import {
+    // pathsToModuleNameMapper,
+
+    type JestConfigWithTsJest,
+} from 'ts-jest';
+
+// import { compilerOptions } from './client/tsconfig.json';
+
+const dummy = '<rootDir>/../../../node_modules/identity-obj-proxy';
+
+const config: JestConfigWithTsJest = {
+    // rootDir: './',
+    roots: ['<rootDir>/client'],
+
+    preset: 'ts-jest',
+    // preset: 'ts-jest/presets/default-esm',
+    // preset: 'ts-jest/presets/js-with-ts',
+    testEnvironment: 'node',
+
+    // modulePaths: [compilerOptions.baseUrl],
+    // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+
+    // modulePaths: ['<rootDir>'],
+    moduleDirectories: [
+        // custom-ui / eo /sources / root
+        '<rootDir>/../../../node_modules',
+
+        '<rootDir>/../../../sources/eo/custom-ui/client',
+        '<rootDir>/../../../sources/eo/custom-ui/core',
+        '<rootDir>/../../../sources/eo/custom-ui/registry',
+
+        '<rootDir>/../../../sources/saby/types',
+        '<rootDir>/../../../sources/sbis/rmi/src/client',
+        '<rootDir>/../../../sources/sbis/ws',
+        '<rootDir>/../../../sources/saby/wasaby-app/src',
+        '<rootDir>/../../../sources/saby/i18n',
+        '<rootDir>/../../../sources/saby/wasaby-requirejs-loader',
+        '<rootDir>/../../../sources/saby/wasaby-controls',
+        '<rootDir>/../../../sources/saby/ui',
+        '<rootDir>/../../../sources/sbis/core',
+
+        // '<rootDir>/../../../builds/online/build-ui/resources',
+    ],
+
+    moduleNameMapper: {
+        '^i18n!.+': '<rootDir>/../../../app/mocks/i18n.ts',
+        '^css!.+': dummy,
+        '^wml!.+': dummy,
+        '^optional!.+': dummy,
+        '^text$': dummy,
+        '^native-css$': dummy,
+        // './Loader': dummy,
+        // '^LocalizationConfigs/.+json$': dummy,
+        // '^UI/Base$': dummy,
+
+        '^Core/(.+)$': '<rootDir>/../../../sources/sbis/ws/WS.Core/core/$1',
+        '^Lib/(.+)$': '<rootDir>/../../../sources/sbis/ws/WS.Core/lib/$1',
+        '^UICore/(.+)$': '<rootDir>/../../../sources/saby/ui/UIReact/UICore/$1',
+    },
+
+    transform: {
+        // '^LocalizationConfigs.+json$': '<rootDir>/../../../app/transformers/json.mjs',
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/client/tsconfig.json',
+                isolatedModules: true,
+                // module: 'amd',
+            },
+        ],
+    },
+
+    setupFiles: ['<rootDir>/../../../app/configs/jest.setup.ts'],
+};
+
+
+export default config;
